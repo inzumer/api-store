8bb83568f81f684a517498fb7560be63107d4ed5
julianactrl <juliana.mg3@gmail.com>
2025-05-25 13:06:28 -0300

    Add paypal module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth';
 import { ProductModule } from './product';
 import { SearchModule } from './search';
 import { UsersModule } from './users';
+import { PaypalModule } from './paypal/paypal.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, SearchModule, UsersModule],
+  imports: [AuthModule, ProductModule, SearchModule, UsersModule, PaypalModule],
   controllers: [],
   providers: [],
 })
