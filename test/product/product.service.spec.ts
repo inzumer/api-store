b7d1a5a273fe3f27dedca6c3446f59f4e6164318
inzumer <inzumer@gmail.com>
2025-07-05 19:10:04 +0200

    fix lint errors

--- test/product/product.service.spec.ts
+++ test/product/product.service.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 /** Nest */
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotFoundException, HttpException } from '@nestjs/common';
